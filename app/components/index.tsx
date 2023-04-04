5bde0e2ae8f753471188d0de47f4903e3bf9def4
Aneeq Khatri <aneeqabdulsamad5761@gmail.com>
2023-04-04 17:11:33 +0500

    metaverser website

---
+++ app/components/index.tsx
@@ -0,0 +1,18 @@
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import ExploreCard from "./ExploreCard";
+import InsightCard from "./InsightCard";
+import NewFeatures from "./NewFeatures";
+import StartSteps from "./StartSteps";
+import { TypingText, TitleText } from "./CustomTexts";
+
+export {
+  Navbar,
+  Footer,
+  ExploreCard,
+  InsightCard,
+  NewFeatures,
+  StartSteps,
+  TypingText,
+  TitleText,
+};
